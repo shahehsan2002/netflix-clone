c3213050274ae8eb4466a2cb35fdec658cc49673
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-14 21:36:26 +0600

    footer created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,34 @@
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+// import twitter_icon from '../../assets/twitter_icon.png'
 
 const Footer = () => {
   return (
-    <div className='footer' >Footer</div>
+    <div className='footer' >
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="youtube_icon" />
+        <img src={facebook_icon} alt="facebook_icon" />
+        <img src={instagram_icon} alt="instagram_icon" />
+        {/* <img src={twitter_icon} alt="twitter_icon" /> */}
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Performances</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text' >@1997-2024 Netflix, Inc</p>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,42 +1,3 @@
-// import "./Home.css";
-// import Navbar from "../../components/Navbar/Navbar";
-// import hero_banner from "../../assets/hero_banner.jpg";
-// import hero_title from "../../assets/hero_title.png";
-// import play_icon from "../../assets/play_icon.png";
-// import info_icon from "../../assets/info_icon.png";
-
-// const Home = () => {
-//   return (
-//     <div className="home">
-//       <Navbar />
-//       {/* Hero   */}
-      
-//       <div className="hero">
-//         <img src={hero_banner} alt="hero_banner" className="banner-img" />
-//         <div className="hero-caption">
-//           <img src={hero_title} alt="hero_title" className="caption-img" />
-//           <p>
-//             Discovering his ties to a secret ancient order, a young man living
-//             in modern Istanbul embarks on a quest to save the city from an
-//             immortal enemy.
-//           </p>
-//           <div className="hero-btns">
-//             <button>
-//               <img className="btn" src={play_icon} alt="play" /> Play
-//             </button>
-//             <button>
-//               <img className="btn dark-btn" src={info_icon} alt="info" /> More
-//               Info
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
@@ -45,6 +6,7 @@ import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
 import TitleCards from "../../components/TitleCards/TitleCards";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -77,6 +39,8 @@ const Home = () => {
       <TitleCards title={"Upcoming"} />
       <TitleCards title={"Top Pics for you"} />
       </div>
+     
+      <Footer/>
     </div>
   );
 };
