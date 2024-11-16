d0a2d42508965a9b79abf3752c7890140a5e779b
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-16 14:53:20 +0600

    login page added in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Home from "./pages/Home/Home"
 import { Routes, Route } from "react-router-dom"
-
+import Login from "./pages/Login/Login"
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
