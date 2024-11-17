0b6bb52a04acffc39e0db5150968b5feeda7dc7c
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-17 12:25:54 +0600

    backgroud image add with blacl layer in login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,26 @@
 import './Login.css'
+import logo from '../../assets/logo.png'
 const Login = () => {
   return (
-    <div className='login' >Login</div>
+    <div className='login' >
+      <img src={logo} alt="logo" className='login-logo' />
+      <div className="div-form">
+        <h1>Sign Up</h1>
+        <form >
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email ' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="Remember Me"></label>
+            </div>
+            <p>Need Help ?</p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
